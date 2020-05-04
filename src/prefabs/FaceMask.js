d7abcbe8a72a26393a518c7ca729246c9e55cd46
Miguelcloid <mreniva@ucsc.edu>
2020-05-04 00:24:49 -0700

    Changed FaceMask to Soap

--- src/prefabs/FaceMask.js
+++ src/prefabs/FaceMask.js
@@ -17,15 +17,15 @@ class FaceMask extends Phaser.GameObjects.Sprite {
              this.randomFloor = getRandomInt(1, 3);
             if(this.randomFloor == 1){
                 this.x = game.config.width;
-                this.y = game.config.height;
+                this.y = game.config.height-100;
             }
             if(this.randomFloor == 2){
                 this.x = game.config.width;
-                this.y = (game.config.height*2)/3;
+                this.y = (game.config.height*2)/3-100;
             }
             if(this.randomFloor == 3){
                 this.x = game.config.width;
-                this.y = game.config.height/3;
+                this.y = game.config.height/3-100;
             }
         }
     }
