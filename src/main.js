f96ccf402a386eafbd96f8360dae44774b8a6f31
Modesto Amador <mramador@ucsc.edu>
2020-05-04 20:29:41 -0700

    Main.js Header added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+/*
+Names of Collaborators: Modesto Amador, Miguelcloid Reniva, Brent Hopkins
+Game Title: Pandemic Runner
+Date Completed: 5/4/2020
+Creative Tilt: We are all fond of our art and music that Brent created to make the atmosphere of the game match our lore.
+               We are also fond of the damage over time mechanic Cloid and Modesto implemented. The damage over time affects
+               not only player health but a health bar UI asset that we created as well. Another technical interesting mechanic
+               we had was showing players their x' distance travelled in the game over screen. Cloid looked through a lot of poor
+               Phaser 3 documentation to figure that out.
+
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 1280,
